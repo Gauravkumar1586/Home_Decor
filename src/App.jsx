888b84b6aab0701e7cc4a797b2b7838c3e10d688
Gaurav Kumar <gauravku1586@gmail.com>
2023-02-24 11:45:26 +0530

    stop crousel

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <Flownavbar></Flownavbar>
-    <Flowcrousel></Flowcrousel>
+    {/* <Flowcrousel></Flowcrousel> */}
     </>
   )
 }
--- src/components/Crouser.jsx
+++ src/components/Crouser.jsx
@@ -2,29 +2,29 @@ import { Carousel } from "flowbite"
 
 export default function Flowcrousel(){
     return(
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-        <Carousel slideInterval={5000}>
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-            alt="..."
-          />
-        </Carousel>
-      </div>
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <Carousel slideInterval={5000}>
+        <img
+          src="f1.jpg"
+          alt="..."
+        />
+        <img
+          src="f2.jpg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+          alt="..."
+        />
+      </Carousel>
+    </div>
     )
 }
